f6bd1c42171c84a8517ee0af171d6379e5a74eb2
DESKTOP-HMV6PSN\user <cus4703@gmail.com>
2020-11-25 21:34:38 +0900

    최초 배포 2020-11-25

---
+++ src/main/java/com/homestaxt/scrap/ScrapApplication.java
@@ -0,0 +1,13 @@
+package com.homestaxt.scrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapApplication.class, args);
+	}
+
+}
